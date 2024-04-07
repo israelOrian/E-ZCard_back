dd2295d4cb9336972b986b8139ce815dd46fb37a
israelOrian <Io4175686@gmail.com>
2024-04-07 13:14:05 +0300

    server working

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express = require('express');
+const http = require('http');
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.json({ msg: 'express work' })
+});
+
+const server = http.createServer(app);
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
